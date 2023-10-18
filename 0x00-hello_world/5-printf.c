a64bd88c383ac0089703e70ffd8b7275ee5dc953
Jeffolefty <ngaojeff4@gmail.com>
2023-10-18 14:14:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prints a line using the printf function.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
